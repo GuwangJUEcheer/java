8ce967e9ff122ab412d5e00a7a1adfa40ff190c3
MYCOMPUTER\17685 <yiyi5205211314@gmail.com>
2024-02-24 16:16:16 +0900

    拦截器提交

---
+++ src/main/java/cn/itcast/mp/config/InterCeptorConfig.java
@@ -0,0 +1,17 @@
+package cn.itcast.mp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import cn.itcast.mp.interceptor.AuthorizeInterceptor;
+
+@Configuration
+public class InterCeptorConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		AuthorizeInterceptor interceptor = new AuthorizeInterceptor();
+		registry.addInterceptor(interceptor);
+	}	
+}
--- src/main/java/cn/itcast/mp/interceptor/AuthorizeInterceptor.java
+++ src/main/java/cn/itcast/mp/interceptor/AuthorizeInterceptor.java
@@ -1,6 +1,32 @@
 package cn.itcast.mp.interceptor;
 
+import java.util.Objects;
 
-public class AuthorizeInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+
+//自定义实现拦截器
+public class AuthorizeInterceptor  implements HandlerInterceptor{
+
+	@Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		
+		//记录登录次数
+		// 获得ip地址
+		String ipAddress = request.getRemoteAddr();
+		// 统计访问次数并且输出
+		HttpSession session = request.getSession();
+		Integer count = Integer.parseInt(session.getAttribute("count").toString());
+		if (!Objects.isNull(count)) {
+			count++;
+		}
+		session.setAttribute("count", count);
+		session.setAttribute("ipAddress", ipAddress);
+    	
+		return true;
+    }
 }
+
